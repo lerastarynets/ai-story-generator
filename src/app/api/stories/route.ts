d504f9ebdcb34b03f722497f418bc831e975624b
valeriiaKoitechs <valeria.koitechs@gmail.com>
2024-09-13 13:56:50 +0300

    feat: add endpoint to fetch all stories

---
+++ src/app/api/stories/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest } from 'next/server';
+
+import { DEFAULT_PAGE, DEFAULT_PER_PAGE } from '../../../lib/constants';
+import prisma from '../../../lib/prisma';
+
+export async function GET(req: NextRequest) {
+  try {
+    const searchParams = req.nextUrl.searchParams;
+    const page = Number(searchParams.get('page')) || DEFAULT_PAGE;
+    const perPage = Number(searchParams.get('perPage')) || DEFAULT_PER_PAGE;
+
+    const stories = await prisma.story.findMany({
+      skip: page * perPage,
+      take: perPage,
+    });
+
+    const storiesCount = await prisma.story.count();
+    return Response.json(
+      {
+        stories,
+        total: storiesCount,
+        page,
+        perPage,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: 'Something went wrong' }, { status: 500 });
+  }
+}
