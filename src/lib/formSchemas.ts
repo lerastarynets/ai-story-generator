d28d2793800e40c23ae789f88886b5d532b56fac
valeriiaKoitechs <valeria.koitechs@gmail.com>
2024-09-13 12:01:11 +0300

    chore: add react-hook-form, yup and schema for story form

---
+++ src/lib/formSchemas.ts
@@ -0,0 +1,11 @@
+import * as yup from 'yup';
+
+export const storySchema = yup.object({
+  character1: yup.string().required('Character 1 is required'),
+  character2: yup.string().required('Character 2 is required'),
+  relationship: yup.string().required('Relationship is required'),
+  events: yup
+    .array(yup.object({ event: yup.string().required('Event is required') }))
+    .min(1, 'At least one event is required')
+    .required(),
+});
