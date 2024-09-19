3f09c3cb45330423464f2c6717d2ade1b9a45188
valeriiaKoitechs <valeria.koitechs@gmail.com>
2024-09-19 13:00:35 +0300

    chore: add saveStorySchema

--- src/lib/formSchemas.ts
+++ src/lib/formSchemas.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
-export const storySchema = yup.object({
+export const generateStorySchema = yup.object({
   character1: yup.string().required('Character 1 is required'),
   character2: yup.string().required('Character 2 is required'),
   relationship: yup.string().required('Relationship is required'),
@@ -8,4 +8,9 @@ export const storySchema = yup.object({
     .array(yup.object({ event: yup.string().required('Event is required') }))
     .min(1, 'At least one event is required')
     .required(),
+});
+
+export const saveStorySchema = yup.object({
+  name: yup.string().required('Name is required'),
+  content: yup.string().required('Text is required'),
 });
