8521ef852ddae05c6361738556eef3860c1ad69e
valeriiaKoitechs <valeria.koitechs@gmail.com>
2024-09-23 16:25:06 +0300

    feat: add page to view each story

---
+++ src/app/stories/[id]/page.tsx
@@ -0,0 +1,30 @@
+import { Box, Typography } from '@mui/material';
+import { notFound } from 'next/navigation';
+
+import { getStoryById } from '../../../server-actions/stories';
+
+const Page = async ({ params }: { params: { id: string } }) => {
+  const story = await getStoryById(params.id);
+
+  if (!story) {
+    notFound();
+  }
+
+  return (
+    <Box className='space-y-4 p-8'>
+      <Typography align='center' variant='h4'>
+        {story.name}
+      </Typography>
+      <Typography variant='body1'>
+        {story.content.split('\n').map((line, index) => (
+          <span key={index}>
+            {line}
+            <br />
+          </span>
+        ))}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Page;
