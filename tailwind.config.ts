d77a91f82fd26282df3b8cb7f26014ec4b10d2b8
valeriiaKoitechs <valeria.koitechs@gmail.com>
2024-10-01 17:59:00 +0300

    chore: update breakpoints in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  corePlugins: {preflight: false},
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  corePlugins: { preflight: false },
+  content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    screens: {
+      xs: '0px',
+      sm: '600px',
+      md: '900px',
+      lg: '1200px',
+      xl: '1440px',
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
