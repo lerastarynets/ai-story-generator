b4668f3a4f6c876fbae0c709388f630fd3f31276
valeriiaKoitechs <valeria.koitechs@gmail.com>
2024-10-25 17:01:50 +0300

    chore: setup middleware

---
+++ src/middleware.ts
@@ -0,0 +1,45 @@
+import NextAuth from 'next-auth';
+
+import authConfig from '@/auth.config';
+import { API_AUTH_PREFIX, AUTH_ROUTES, DEFAULT_LOGIN_REDIRECT, PUBLIC_ROUTES } from '@/routes';
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl, auth } = req;
+
+  const isApiAuthPrefix = nextUrl.pathname.startsWith(API_AUTH_PREFIX);
+  const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname);
+  const isAuthRoute = AUTH_ROUTES.includes(nextUrl.pathname);
+
+  const isLoggedIn = !!auth;
+
+  if (isApiAuthPrefix) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL('/login', nextUrl));
+  }
+
+  console.log('PATH', req.nextUrl.pathname);
+  console.log('IS_LOGGED_IN', isLoggedIn);
+
+  return;
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+export const AUTH_ROUTES = ['/login', '/signup'];
+
+export const API_AUTH_PREFIX = '/api/auth';
+
+export const DEFAULT_LOGIN_REDIRECT = '/generate';
+
+export const PUBLIC_ROUTES = ['/'];
