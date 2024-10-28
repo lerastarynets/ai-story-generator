8f73e6c7546bed20a9592623a2d77f6e7ae98520
valeriiaKoitechs <valeria.koitechs@gmail.com>
2024-10-28 16:40:23 +0200

    chore: add Credentials provider to auth.config.js

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,3 +1,35 @@
 import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 
-export default { providers: [] } satisfies NextAuthConfig;
+import { logInSchema } from '@/lib/formSchemas';
+import { verifyPassword } from '@/lib/helpers';
+import { getUserByEmail } from '@/server-actions/user';
+
+export default {
+  providers: [
+    Credentials({
+      authorize: async (credentials) => {
+        const validatedData = await logInSchema.validate(credentials);
+
+        if (validatedData) {
+          const { email, password } = validatedData;
+
+          const { data: user } = await getUserByEmail(email);
+
+          if (!user || !user.password) {
+            return null;
+          }
+
+          const isValidPassword = await verifyPassword(password, user.password);
+
+          if (!isValidPassword) {
+            return null;
+          }
+
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
