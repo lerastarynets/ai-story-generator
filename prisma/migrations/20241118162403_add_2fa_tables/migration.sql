494dd834fd09cb58896813d44efc5f38ad8a9798
valeriiaKoitechs <valeria.koitechs@gmail.com>
2024-11-20 17:35:40 +0200

    chore: add TwoFactor schemas

---
+++ prisma/migrations/20241118162403_add_2fa_tables/migration.sql
@@ -0,0 +1,32 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "is2FAEnabled" BOOLEAN NOT NULL DEFAULT false;
+
+-- CreateTable
+CREATE TABLE "two_factor_tokens" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "two_factor_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "two_factor_confirmations" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "two_factor_confirmations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "two_factor_tokens_token_key" ON "two_factor_tokens"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "two_factor_tokens_email_token_key" ON "two_factor_tokens"("email", "token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "two_factor_confirmations_userId_key" ON "two_factor_confirmations"("userId");
+
+-- AddForeignKey
+ALTER TABLE "two_factor_confirmations" ADD CONSTRAINT "two_factor_confirmations_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
