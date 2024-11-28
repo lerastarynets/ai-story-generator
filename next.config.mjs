d15028b247b6c313ae0d3ea7dfa44e040d006fbb
valeriiaKoitechs <valeria.koitechs@gmail.com>
2024-11-28 17:54:16 +0200

    chore: add env vars to next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
     env: {
         DATABASE_URL: process.env.DATABASE_URL ?? "",
+        SALT_ROUNDS: process.env.SALT_ROUNDS ?? "",
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY ?? "",
+        RESEND_API_KEY: process.env.RESEND_API_KEY ?? "",
+        AUTH_SECRET: process.env.AUTH_SECRET ?? "",
+        DATABASE_URL: process.env.DATABASE_URL ?? "",
     },
     reactStrictMode: false,
 };
