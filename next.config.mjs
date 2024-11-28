034047e61b898d1579f842ee7137cf5979a92291
valeriiaKoitechs <valeria.koitechs@gmail.com>
2024-11-28 18:05:18 +0200

    chore: add NEXTAUTH_URL to next config

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
         OPENAI_API_KEY: process.env.OPENAI_API_KEY ?? "",
         RESEND_API_KEY: process.env.RESEND_API_KEY ?? "",
         AUTH_SECRET: process.env.AUTH_SECRET ?? "",
-        DATABASE_URL: process.env.DATABASE_URL ?? "",
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL ?? "",
     },
     reactStrictMode: false,
 };
