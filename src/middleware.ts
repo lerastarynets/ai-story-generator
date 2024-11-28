bc5c3cc94ace5ad0789f64c9b5cfdf80c07901e5
valeriiaKoitechs <valeria.koitechs@gmail.com>
2024-11-28 18:59:16 +0200

    chore: event loggs

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,7 +18,7 @@ export default auth((req) => {
     return;
   }
 
-  console.log(isLoggedIn, isAuthRoute, isPublicRoute, nextUrl);
+  console.log(auth, req.cookies, isAuthRoute, isPublicRoute, nextUrl);
   if (isAuthRoute) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
